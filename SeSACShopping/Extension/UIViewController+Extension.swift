87d8c1105095f491b4442dd06f22977eb717b6a5
chasomin <somin49@naver.com>
2024-02-06 19:10:09 +0900

    toast 추가, showToast 함수 구현

--- SeSACShopping/Extension/UIViewController+Extension.swift
+++ SeSACShopping/Extension/UIViewController+Extension.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Toast
 
 extension UIViewController {
     func showAlert(title: String, message: String, buttonTitle: String, completionHandler: @escaping () -> Void) {
@@ -24,5 +25,14 @@ extension UIViewController {
         alert.view.tintColor = .point
         
         present(alert, animated: true)
+    }
+    
+    func showToast(view: UIView, message: String) {
+        var style = ToastStyle()
+        style.backgroundColor = .white
+        style.messageColor = .black
+        style.messageAlignment = .center
+        view.makeToast(message, duration: 2.0, position: .top, style: style)
+
     }
 }
