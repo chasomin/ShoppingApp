7b5add452729eb504a025568c62a561cd335a822
chasomin <somin49@naver.com>
2024-02-06 21:21:29 +0900

    CircleImageButton Custom 생성

--- SeSACShopping/CustomView/CircleImageButton.swift
+++ SeSACShopping/CustomView/CircleImageButton.swift
@@ -5,4 +5,30 @@
 //  Created by 차소민 on 2/6/24.
 //
 
-import Foundation
+import UIKit
+
+class CircleImageButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    func configureView() {
+        self.layer.borderColor = UIColor.point.cgColor
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
